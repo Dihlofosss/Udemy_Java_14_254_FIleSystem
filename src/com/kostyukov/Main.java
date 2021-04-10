3185868cf18df6eb09dc4044e600561252b44b46
Kostyukov <maxim.kostyukov@googlemail.com>
2021-04-10 14:25:42 +0200

    ./Examples File's copies

--- src/com/kostyukov/Main.java
+++ src/com/kostyukov/Main.java
@@ -1,6 +1,5 @@
 package com.kostyukov;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -10,30 +9,19 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-		printFile(path);
-		System.out.println(path.toAbsolutePath());
-		path = FileSystems.getDefault().getPath("files\\SubdirectoryFile.txt");
-		printFile(path);
-		System.out.println(path.toAbsolutePath());
-		path = FileSystems.getDefault().getPath("C:\\Projects\\Java\\OutsideLocalFile.txt");
-		printFile(path);
-		System.out.println(path.toAbsolutePath());
-	}
-	
-	private static void printFile(Path path)
-	{
-		try(BufferedReader fileReader = Files.newBufferedReader(path))
+		try
 		{
-			String line;
-			while ((line = fileReader.readLine()) != null)
+			Path sourceFile = FileSystems.getDefault().getPath("Examples/file1.txt");
+			Path destFIle = FileSystems.getDefault().getPath("Examples/file1_copy.txt");
+			
+			if (Files.exists(sourceFile))
 			{
-				System.out.println(line);
+				Files.copy(sourceFile,destFIle);
+				System.out.println("File: " + sourceFile.toAbsolutePath() + " is copied successfully");
 			}
 		}
 		catch (IOException e)
 		{
-			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
