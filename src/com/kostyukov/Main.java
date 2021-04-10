1c699cd462a10da47170948d4b5c850d1d2c276f
Kostyukov <maxim.kostyukov@googlemail.com>
2021-04-10 15:12:55 +0200

    File overwrite request

--- src/com/kostyukov/Main.java
+++ src/com/kostyukov/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 public class Main
 {
@@ -16,12 +18,23 @@ public class Main
 			
 			if (Files.exists(sourceFile) && Files.notExists(destFIle))
 			{
-				Files.copy(sourceFile,destFIle);
+				Files.copy(sourceFile, destFIle);
 				System.out.println("File: " + sourceFile.toAbsolutePath() + " is copied successfully");
+			}
+			else
+			{
+				System.out.println("File: " + destFIle.toAbsolutePath() + " is already exists. \nOverwrite? Y/N");
+				Scanner scanner = new Scanner(System.in);
+				if (scanner.nextLine().equalsIgnoreCase("y"))
+				{
+					Files.copy(sourceFile, destFIle, StandardCopyOption.REPLACE_EXISTING);
+					System.out.println("File is overwritten");
+				}
 			}
 		}
 		catch (IOException e)
 		{
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
